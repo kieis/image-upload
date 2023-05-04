66151a2d93d6b1a4f8000e24212d2b03d5bb472e
Dirlan Ferreira <dirlan2014@yahoo.com.br>
2023-05-04 05:33:27 -0300

    chore: upload

--- src/components/Form/FormAddImage.tsx
+++ src/components/Form/FormAddImage.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable prettier/prettier */
 import { Box, Button, Stack, useToast } from '@chakra-ui/react';
-import { useForm } from 'react-hook-form';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import { useState } from 'react';
 import { useMutation, useQueryClient } from 'react-query';
 
@@ -11,6 +13,12 @@ interface FormAddImageProps {
   closeModal: () => void;
 }
 
+interface ImageFormData {
+  url: string;
+  title: string;
+  description: string;
+}
+
 export function FormAddImage({ closeModal }: FormAddImageProps): JSX.Element {
   const [imageUrl, setImageUrl] = useState('');
   const [localImageUrl, setLocalImageUrl] = useState('');
@@ -18,43 +26,97 @@ export function FormAddImage({ closeModal }: FormAddImageProps): JSX.Element {
 
   const formValidations = {
     image: {
-      // TODO REQUIRED, LESS THAN 10 MB AND ACCEPTED FORMATS VALIDATIONS
+      required: {
+        value: true,
+        message: 'Required image',
+      },
+      validate: {
+        lessThan10MB: file => file[0]?.size < 10000000 || 'File max size: 10MB',
+        acceptedFormats: file =>
+          ['image/jpeg', 'image/png', 'image/gif'].includes(file[0]?.type) ||
+          'Accepting only PNG, JPEG e GIF',
+      },
     },
     title: {
-      // TODO REQUIRED, MIN AND MAX LENGTH VALIDATIONS
+      required: {
+        value: true,
+        message: 'Required title',
+      },
+      minLength: {
+        value: 2,
+        message: 'Min characters accepted: 2.',
+      },
+      maxLength: {
+        value: 20,
+        message: 'Max characters accepted: 20.',
+      },
     },
     description: {
-      // TODO REQUIRED, MAX LENGTH VALIDATIONS
+      required: {
+        value: true,
+        message: 'Required description.',
+      },
+      maxLength: {
+        value: 65,
+        message: 'Max characters accepted: 65.',
+      },
     },
   };
 
   const queryClient = useQueryClient();
-  const mutation = useMutation(
-    // TODO MUTATION API POST REQUEST,
+  const addImage = useMutation(
+    async (data: ImageFormData) => {
+      const response = await api.post('api/images', {
+        url: imageUrl,
+        title: data.title,
+        description: data.description,
+      });
+      return response.data;
+    },
+
     {
-      // TODO ONSUCCESS MUTATION
+      onSuccess: () => {
+        queryClient.invalidateQueries('images');
+      },
     }
   );
 
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState,
-    setError,
-    trigger,
-  } = useForm();
+  const { register, handleSubmit, reset, formState, setError, trigger } =
+    useForm();
   const { errors } = formState;
 
-  const onSubmit = async (data: Record<string, unknown>): Promise<void> => {
+  const onSubmit: SubmitHandler<ImageFormData> = async data => {
     try {
-      // TODO SHOW ERROR TOAST IF IMAGE URL DOES NOT EXISTS
-      // TODO EXECUTE ASYNC MUTATION
-      // TODO SHOW SUCCESS TOAST
+      !imageUrl
+        ? toast({
+            title: 'Image not uploaded successfully',
+            description: 'Needed wait image be uploaded.',
+            status: 'error',
+            duration: 9000,
+            isClosable: true,
+          })
+        : await addImage.mutateAsync(data);
+
+      toast({
+        title: 'Image uploaded successfully',
+        description: 'Your image got upload successfully.',
+        status: 'success',
+        duration: 9000,
+        isClosable: true,
+      });
     } catch {
-      // TODO SHOW ERROR TOAST IF SUBMIT FAILED
+      toast({
+        title: 'Image upload failed',
+        description: 'Something went wrong uploading your image.',
+        status: 'error',
+        duration: 9000,
+        isClosable: true,
+      });
     } finally {
-      // TODO CLEAN FORM, STATES AND CLOSE MODAL
+      reset();
+      setImageUrl(null);
+      setLocalImageUrl(null);
+      closeModal();
     }
   };
 
@@ -67,20 +129,20 @@ export function FormAddImage({ closeModal }: FormAddImageProps): JSX.Element {
           setLocalImageUrl={setLocalImageUrl}
           setError={setError}
           trigger={trigger}
-          // TODO SEND IMAGE ERRORS
-          // TODO REGISTER IMAGE INPUT WITH VALIDATIONS
+          error={errors.image}
+          {...register('image', formValidations.image)}
         />
 
         <TextInput
           placeholder="Título da imagem..."
-          // TODO SEND TITLE ERRORS
-          // TODO REGISTER TITLE INPUT WITH VALIDATIONS
+          error={errors.title}
+          {...register('title', formValidations.title)}
         />
 
         <TextInput
           placeholder="Descrição da imagem..."
-          // TODO SEND DESCRIPTION ERRORS
-          // TODO REGISTER DESCRIPTION INPUT WITH VALIDATIONS
+          error={errors.description}
+          {...register('description', formValidations.description)}
         />
       </Stack>
 
--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import {
   Modal,
   ModalOverlay,
@@ -19,5 +20,53 @@ export function ModalViewImage({
   onClose,
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
-  // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+  return (
+    <>
+      <Modal onClose={onClose} isOpen={isOpen} isCentered>
+        <ModalOverlay bg="#1B1A185e" />
+        <ModalContent>
+          <ModalBody
+            x="900px"
+            y="600px"
+            bg="pGray.900"
+            w="100%"
+            h="100%"
+            mx="0"
+            my="0"
+            px="0"
+            py="0"
+            borderRadius={15}
+          >
+            <Image src={imgUrl} h="100%" w="100%" mx={0} my={0} py={0} px={0} />
+          </ModalBody>
+          <ModalFooter
+            bg="pGray.700"
+            fontWeight="pGray.400"
+            mx="0"
+            my="0"
+            px="1rem"
+            py="1rem"
+            h="4rem"
+            w="100%"
+            justifyContent="left"
+          >
+            <Link
+              href={imgUrl}
+              isExternal
+              w="auto"
+              h="auto"
+              mx="0"
+              my="0"
+              px={2}
+              py={1}
+              textAlign="left"
+              borderRadius={10}
+            >
+              Abrir original
+            </Link>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
 }
